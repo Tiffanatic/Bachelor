ab4838e96769023637588962398b7ebf0ca5eed4
Mads <madsrynord@live.dk>
2021-04-27 08:32:57 +0200

    More clean up

--- RapidTime.Core/Services/IContactService.cs
+++ RapidTime.Core/Services/IContactService.cs
@@ -1,7 +1,14 @@
-﻿namespace RapidTime.Core.Services
+﻿using System.Collections.Generic;
+using RapidTime.Core.Models;
+
+namespace RapidTime.Core.Services
 {
     public interface IContactService
     {
-        
+        IEnumerable<Contact> GetAll();
+        void Delete(int contactId);
+        Contact FindById(int i);
+        void Update(Contact contact);
+        void Insert(Contact contact);
     }
 }
