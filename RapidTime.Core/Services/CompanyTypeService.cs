5cba9732977780d025c98af25e426177e8c2c71a
Mads <madsrynord@live.dk>
2021-04-27 14:25:20 +0200

    Added CompanyTypeService with room for adding unit tests later

---
+++ RapidTime.Core/Services/CompanyTypeService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using RapidTime.Core.Models;
+
+namespace RapidTime.Core.Services
+{
+    public class CompanyTypeService : ICompanyTypeService
+    {
+        private IUnitofWork _unitofWork;
+
+        public CompanyTypeService(IUnitofWork unitofWork)
+        {
+            _unitofWork = unitofWork;
+        }
+
+        public IEnumerable<CompanyType> GetAll()
+        {
+            return _unitofWork.CompanyTypeRepository.getAll();
+        }
+
+        public void Delete(int companyTypeId)
+        {
+            if (companyTypeId == null) throw new NullReferenceException();
+            try
+            {
+                _unitofWork.CompanyTypeRepository.Delete(companyTypeId);
+                _unitofWork.Commit();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public CompanyType findById(int i)
+        {
+            return _unitofWork.CompanyTypeRepository.GetbyId(i);
+        }
+
+        public void Update(CompanyType companyType)
+        {
+            try
+            {
+                _unitofWork.CompanyTypeRepository.Update(companyType);
+                _unitofWork.Commit();
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException(e.Message);
+            }
+        }
+
+        public void Insert(CompanyType companyType)
+        {
+            try
+            {
+                _unitofWork.CompanyTypeRepository.Insert(companyType);
+                _unitofWork.Commit();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+        
+    }
+    
+}
---
+++ RapidTime.Core/Services/ICompanyTypeService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using RapidTime.Core.Models;
+
+namespace RapidTime.Core.Services
+{
+    public interface ICompanyTypeService
+    {
+        IEnumerable<CompanyType> GetAll();
+        void Delete(int companyTypeId);
+        CompanyType findById(int i);
+        void Update(CompanyType companyType);
+        void Insert(CompanyType companyType);
+    }
+}
