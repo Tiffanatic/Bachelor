b85ccf857194e4dd863bb4d379ba189eadc94bf9
Jesper Henriksen <jesperhen89@gmail.com>
2021-04-28 14:07:29 +0200

    Changes to assignment model for "DateStarted" instead of date.

--- RapidTime.Core/Models/Assignment.cs
+++ RapidTime.Core/Models/Assignment.cs
@@ -1,13 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RapidTime.Core.Models
 {
     public class Assignment : BaseEntity
     {
         public AssignmentType AssignmentType { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime DateStarted { get; set; }
         public double Amount { get; set; }
-        public string TimeSpent { get; set; }
+        public TimeSpan TimeSpentInTotal { get; set; }
         public Customer Customer { get; set; }
+        public List<TimeRecord> TimeRecords { get; set; }
     }
 }
