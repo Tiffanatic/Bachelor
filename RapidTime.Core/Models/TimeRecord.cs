e705584d4d97152fb1b8487900c8a6ebf63e8d89
Jesper Henriksen <jesperhen89@gmail.com>
2021-05-03 13:07:46 +0200

    new model for TimeRecord to utilize TimeSpan type for summarizing time registered.

--- RapidTime.Core/Models/TimeRecord.cs
+++ RapidTime.Core/Models/TimeRecord.cs
@@ -2,7 +2,7 @@
 
 namespace RapidTime.Core.Models
 {
-    public class TimeRecord
+    public class TimeRecord : BaseEntity
     {
         public TimeSpan TimeRecorded { get; set; }
         public DateTime Date { get; set; }
