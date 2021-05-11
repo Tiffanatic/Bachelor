6aeb1c41864710515f9a1a3ab3a3d706cda649de
Jesper Henriksen <jesperhen89@gmail.com>
2021-05-11 11:04:15 +0200

    Forgot the interface :o)

--- RapidTime.Core/IUserService.cs
+++ RapidTime.Core/IUserService.cs
@@ -1,7 +1,13 @@
-﻿namespace RapidTime.Core
+﻿using System.Threading.Tasks;
+using RapidTime.Core.Models.Auth;
+
+namespace RapidTime.Core
 {
     public interface IUserService
     {
-        
+        Task<User> CreateUser(User input);
+        void DeleteUser(int id);
+        Task<User> UpdateUser(User input);
+        Task<User> GetUser(int id);
     }
 }
