0301c32834776462f01abd6ec4654384957379c6
Mads <madsrynord@live.dk>
2021-05-12 10:33:49 +0200

    Added Proto and initial items for making services

---
+++ RapidTime.Frontend/Models/AuthenticationUserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RapidTime.Frontend.Models
+{
+    public class AuthenticationUserModel
+    {
+        [Required(ErrorMessage ="Email is Required")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password is Required")]
+        public string Password { get; set; }
+
+    }
+}
