e3bfe09bfe7e2c0bbc35055cca34bdc69ba13f98
Jesper Henriksen <jesperhen89@gmail.com>
2021-05-13 14:04:26 +0200

    serilog implemented with multiple destinations.

--- RapidTime.Api/Program.cs
+++ RapidTime.Api/Program.cs
@@ -1,6 +1,8 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 namespace RapidTime.Api
 {
@@ -18,7 +20,25 @@ namespace RapidTime.Api
         // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                // .UseSerilog()
+                .UseSerilog((context, configuration) =>
+                {
+                    configuration.Enrich.FromLogContext()
+                        .Enrich.WithMachineName()
+                        .WriteTo.Console()
+                        .WriteTo.Elasticsearch(
+                            new ElasticsearchSinkOptions(
+                                new Uri(context.Configuration["ElasticConfiguration:Uri"]))
+                            {
+                                //Index format configured with appsettings.json settings and datetime.utcnow() seperated into an index for each month.
+                                IndexFormat =
+                                    $"{context.Configuration["ApplicationName"]}-logs-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}",
+                                AutoRegisterTemplate = true,
+                                NumberOfShards = 2,
+                                NumberOfReplicas = 1
+                            })
+                        .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+                        .ReadFrom.Configuration(context.Configuration);
+                })
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
