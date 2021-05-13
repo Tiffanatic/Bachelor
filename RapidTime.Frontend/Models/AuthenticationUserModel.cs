5a31046304ad22dff370091528a32822d4d41e38
Mads <madsrynord@live.dk>
2021-05-13 10:51:19 +0200

    Added City management on Frontend

--- RapidTime.Frontend/Models/AuthenticationUserModel.cs
+++ RapidTime.Frontend/Models/AuthenticationUserModel.cs
@@ -10,7 +10,7 @@ namespace RapidTime.Frontend.Models
     {
         [Required(ErrorMessage ="Email is Required")]
         public string Email { get; set; }
-        [Required(ErrorMessage = "Password is Required")]
+        
         public string Password { get; set; }
 
     }
