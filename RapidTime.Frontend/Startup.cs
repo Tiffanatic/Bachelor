f85f7ca838741f11d681f3cd849bd0ae395e119c
Mads <madsrynord@live.dk>
2021-05-13 13:06:32 +0200

    Added AddressAggregate Create in frontend

--- RapidTime.Frontend/Startup.cs
+++ RapidTime.Frontend/Startup.cs
@@ -34,6 +34,7 @@ namespace RapidTime.Frontend
             services.AddScoped<StateContainer>();
             services.AddScoped<CountryGrpcClient>();
             services.AddScoped<CityGrpcClient>();
+            services.AddScoped<AddressGrpcClient>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
