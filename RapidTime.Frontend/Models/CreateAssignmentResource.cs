79bb5289e3284ea267be596a6f92c8794a255606
Mads <madsrynord@live.dk>
2021-06-02 16:00:06 +0200

    Create Assignment Done

---
+++ RapidTime.Frontend/Models/CreateAssignmentResource.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace RapidTime.Frontend.Models
+{
+    public class CreateAssignmentResource
+    {
+        public DateTime DateStarted { get; set; }
+        public AssignmentTypeBase AssignmentType { get; set; }
+    }
+}
