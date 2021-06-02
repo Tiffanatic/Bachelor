42be1f9b07ecdc088e9308dacc04b51807ff1478
Jesper Henriksen <jesperhen89@gmail.com>
2021-06-02 06:44:59 +0200

    initial implementation

---
+++ RapidTime.Frontend/ProtoClients/CustomerGrpcClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RapidTime.Frontend.ProtoClients
+{
+    public class CustomerGrpcClient
+    {
+
+    }
+}
